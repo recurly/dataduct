1c47dda0159900eafa673f306d094120e160f824
Paul Porter <pporter@recurly.com>
2018-10-16 09:48:16 -0600

    Added optional expected_value parameter

--- dataduct/steps/executors/custom_check.py
+++ dataduct/steps/executors/custom_check.py
@@ -10,12 +10,14 @@ def get_redshift_data(sql):
     return data.iloc[0][0]
 
 
-def validate_redshift_result(sql, min_result, max_result):
+def validate_redshift_result(sql, min_result, max_result, expected_result):
     result = get_redshift_data(sql)
 
     if min_result and result < min_result:
         raise Exception("result {0} is less than min {1}".format(result, min_result))
+
     if max_result and result > max_result:
         raise Exception("result {0} is greater than max {1}".format(result, max_result))
 
-    print result
+    if expected_result and result != expected_result
+        raise Exception("result {0} is not equal to expected result {1}".format(result, expected_result))
